e016d97f8d7158d12d279bb55f00c4793830c6d3
emsk <emsk1987@gmail.com>
2017-01-31 13:25:21 +0900

    Build NSIS installer instead of Squirrel.Windows
    * NSIS is the default on `electron-builder`
    * The installer size gets smaller (52.8MB -> 31.8MB)
    * The installation time gets faster
    * Install not to `C:\Users\[UserName]\AppData\Local\RedmineNow\app-[AppVersion]`, but to `C:\Users\[UserName]\AppData\Local\Programs\RedmineNow`
    
    See https://github.com/electron-userland/electron-builder/issues/472

--- bin/pack-all.sh
+++ bin/pack-all.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 $(npm bin)/build --mac --win --x64
+
 mv './dist/mac/Redmine Now-0.3.0.dmg' ./dist/mac/RedmineNowSetup-0.3.0.dmg
-mv './dist/win/Redmine Now Setup 0.3.0.exe' ./dist/win/RedmineNowSetup-0.3.0.exe
+
+mkdir -p ./dist/win
+mv './dist/Redmine Now Setup 0.3.0.exe' ./dist/win/RedmineNowSetup-0.3.0.exe
+mv './dist/latest.yml' ./dist/win
--- bin/pack-win.sh
+++ bin/pack-win.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 $(npm bin)/build --win --x64
-mv './dist/win/Redmine Now Setup 0.3.0.exe' ./dist/win/RedmineNowSetup-0.3.0.exe
+mkdir -p ./dist/win
+mv './dist/Redmine Now Setup 0.3.0.exe' ./dist/win/RedmineNowSetup-0.3.0.exe
+mv './dist/latest.yml' ./dist/win
