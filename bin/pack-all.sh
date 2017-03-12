14816b8b8974e48b9842c626cb0ea6619b5f6681
emsk <emsk1987@gmail.com>
2017-03-12 22:20:08 +0900

    Update electron-builder dependency to 15.3.0

--- bin/pack-all.sh
+++ bin/pack-all.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0)/..
 $(yarn bin)/build --mac --win --x64
 
-mv './dist/mac/Redmine Now-0.4.0.dmg' ./dist/mac/RedmineNowSetup-0.4.0.dmg
+mv './dist/Redmine Now-0.4.0.dmg' ./dist/mac/RedmineNowSetup-0.4.0.dmg
 
 mkdir -p ./dist/win
 mv './dist/Redmine Now Setup 0.4.0.exe' ./dist/win/RedmineNowSetup-0.4.0.exe
