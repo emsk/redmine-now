9cb63ef70ac39f549cfffc7086175754bbd048f5
emsk <emsk1987@gmail.com>
2018-04-01 17:51:50 +0900

    Add `--publish never` option to `build` command
    Fix error on Travis CI
    https://travis-ci.org/emsk/redmine-now/builds/360819929#L138

--- bin/pack-win.sh
+++ bin/pack-win.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
-$(yarn bin)/build --win --x64
+$(yarn bin)/build --win --x64 --publish never
 mkdir -p ./dist/win
 mv './dist/Redmine Now Setup 0.4.0.exe' ./dist/win/RedmineNowSetup-0.4.0.exe
 mv './dist/Redmine Now Setup 0.4.0.exe.blockmap' ./dist/win
--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 if [ ! -e './dist/mac/Redmine Now.app' ]; then
-  $(yarn bin)/build --mac --x64
+  $(yarn bin)/build --mac --x64 --publish never
 fi
 $(yarn bin)/ava
